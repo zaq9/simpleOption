2e699a9de0fa7a0609c9de82ae9ce2470919cc3f
zaq9 <zaq_9@yahoo.co.jp>
2019-01-23 23:33:29 +0900

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+    name='simpleOption',
+    version='0.1.0',
+    description='Simple Option module(using QuantLib)',
+    url='https://github.com/zaq9/simpleOption',
+    author='zaq',
+    author_email='zaq_9@yahoo.co.jp',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+    ],
+    keywords=['options','python','finance'],
+    install_requires=['pandas'],
+)

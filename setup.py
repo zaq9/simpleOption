cc520a8897a20b65cc00fdfb0e66714a32adbadc
zaq9 <zaq_9@yahoo.co.jp>
2019-01-24 03:17:33 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='simpleOption',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple Option module(using QuantLib)',
     url='https://github.com/zaq9/simpleOption',
     author='zaq',

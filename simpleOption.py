e44d72c73c09fb504a7d4aea4d5a1181b10dce30
zaq9 <zaq_9@yahoo.co.jp>
2019-02-05 11:52:48 +0900

    add year-mode(sqDate)

--- simpleOption.py
+++ simpleOption.py
@@ -54,25 +54,37 @@ from parse import parse
 
 
 class Date(qb.Date):
-    def __init__(self, intDate):
-        self.name = str(intDate)
-        year = int(self.name[0:4])
-        month = int(self.name[4:6])
-        date = int(self.name[6:8])
-        super().__init__(date, month, year)
+	def __init__(self, intDate):
+		self.name = str(intDate)
+		year = int(self.name[0:4])
+		month = int(self.name[4:6])
+		date = int(self.name[6:8])
+		super().__init__(date, month, year)
 
 
 def sqDate(strSQDate):
-    """Returns :SQ_Date(qb.Date)
-    Parameters
-    type1(monthly):  '02'
-    type2(weekly) :  '02w3'
-    """
+	"""Returns :SQ_Date(qb.Date)
+	Parameters :StrSQDate str
+		type1(monthly):  '02'
+		type2(weekly) :  '02w3'
+		type3(with year): '201102'
+	"""
 
-    month = int(strSQDate[0:2])
-    week = 2 if len(strSQDate) < 2 else int(strSQDate[-1])
-    year = qb.Date.todaysDate().year()  # todo:check otherCase
-    return qb.Date.nthWeekday(week, 6, month, year)
+	year = 0
+	month = 0
+	week = 2
+
+	if len(strSQDate) >= 6:  # type(201902,201904w1)
+		year = int(strSQDate[0:4])
+		s = strSQDate[4:]
+	else:
+		year = qb.Date.todaysDate().year()
+		s = strSQDate
+
+	month = int(s[0:2])
+	week = int(s[-1]) if (len(s) > 2) else 2
+
+	return qb.Date.nthWeekday(week, 6, month, year)
 
 
 # init marketData
@@ -85,21 +97,21 @@ def setEvaluationDate(date): qb.Settings.instance().setEvaluationDate(date)
 
 
 def setting(
-        underlying=None,
-        iv=None,
-        evaluationDate=None,
-        rate=None):
-    """ Market Data Setting """
-    if underlying is not None: u.setValue(float(underlying))
-    if iv is not None: sigma.setValue(float(iv) / 100)
-    if rate is not None: r.setValue(float(rate))
-    if evaluationDate is not None: setEvaluationDate(Date(evaluationDate))
+		underlying=None,
+		iv=None,
+		evaluationDate=None,
+		rate=None):
+	""" Market Data Setting """
+	if underlying is not None: u.setValue(float(underlying))
+	if iv is not None: sigma.setValue(float(iv) / 100)
+	if rate is not None: r.setValue(float(rate))
+	if evaluationDate is not None: setEvaluationDate(Date(evaluationDate))
 
 
 def view():
-    print(f"EvaluationDate={qb.Settings.instance().evaluationDate}")
-    print(f"u={u.value()}")
-    print(f"iv={sigma.value()}")
+	print(f"EvaluationDate={qb.Settings.instance().evaluationDate}")
+	print(f"u={u.value()}")
+	print(f"iv={sigma.value()}")
 
 
 riskFreeCurve = qb.FlatForward(0, qb.Japan(), qb.QuoteHandle(r), qb.Actual360())
@@ -111,38 +123,38 @@ engine = qb.AnalyticEuropeanEngine(process)
 
 
 class Payoff(qb.PlainVanillaPayoff):
-    def __init__(self, strPayoff):
-        self.name = strPayoff
-        x = parse("{op_type}{strike:d}", strPayoff)
-        super().__init__(
-            qb.Option.Call if x['op_type'] == 'C' else qb.Option.Put, x['strike'])
+	def __init__(self, strPayoff):
+		self.name = strPayoff
+		x = parse("{op_type}{strike:d}", strPayoff)
+		super().__init__(
+			qb.Option.Call if x['op_type'] == 'C' else qb.Option.Put, x['strike'])
 
-    def __str__(self): return self.name
+	def __str__(self): return self.name
 
 
 class Option(qb.EuropeanOption):
-    def __init__(self, strOption):
-        self.name = strOption
-        x = parse("{sq_date}/{payoff}", strOption)
-        self.payoff = Payoff(x['payoff'])
-        super().__init__(
-            self.payoff, qb.EuropeanExercise(sqDate(x['sq_date'])))
-        self.setPricingEngine(engine)
+	def __init__(self, strOption):
+		self.name = strOption
+		x = parse("{sq_date}/{payoff}", strOption)
+		self.payoff = Payoff(x['payoff'])
+		super().__init__(
+			self.payoff, qb.EuropeanExercise(sqDate(x['sq_date'])))
+		self.setPricingEngine(engine)
 
-    def __str__(self): return self.name
+	def __str__(self): return self.name
 
-    def v(self, underlying=None, iv=None, evoluationDate=None):
-        setting(underlying, iv, evoluationDate)
-        return self.NPV()
+	def v(self, *setting_args):
+		setting(*setting_args)
+		return self.NPV()
 
-    def pay(self, underlying): return self.payoff(float(underlying))
+	def pay(self, underlying): return self.payoff(float(underlying))
 
 
 class Portfolio():
-    """簡単なポートフォリオクラス"""
+	"""簡単なポートフォリオクラス"""
 
-    def __init__(self, lines):
-        '''
+	def __init__(self, lines):
+		'''
 		:param lines: str
 
 		Example:
@@ -153,16 +165,16 @@ class Portfolio():
 				02/C21500[1]
 			""")
 		'''
-        self.items = []
-        self.nums = []
-        for s in lines.strip().splitlines():
-            x = parse("{str_op}[{num:d}]", s.strip())
-            self.items.append(Option(x['str_op']))
-            self.nums.append(x['num'])
+		self.items = []
+		self.nums = []
+		for s in lines.strip().splitlines():
+			x = parse("{str_op}[{num:d}]", s.strip())
+			self.items.append(Option(x['str_op']))
+			self.nums.append(x['num'])
 
-    def v(self, underlying=None, iv=None, evoluationDate=None):
-        setting(underlying, iv, evoluationDate)
-        return sum(op.v() * num for op, num in zip(self.items, self.nums))
+	def v(self, *setting_args):
+		setting(*setting_args)
+		return sum(op.v() * num for op, num in zip(self.items, self.nums))
 
-    def pay(self, underlying):
-        return sum(op.pay(underlying) * num for op, num in zip(self.items, self.nums))
+	def pay(self, underlying):
+		return sum(op.pay(underlying) * num for op, num in zip(self.items, self.nums))
